102b00a92397587a504bf8ff8ffbc77da0415cc8
kristian ortiz <kortizl88@gmail.com>
2020-12-05 18:14:29 -0600

    Cambios de la fase 1

---
+++ app-prueba/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cambios de la fase 1</h1>
+      <h2>estos son los cambios </h2>
+      <h3>Fin</h3>
+    </div>
+  );
+}
+
+export default App;
