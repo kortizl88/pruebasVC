b55d591cade489b85206df9d9003c0a7c9bd4ac7
kristian ortiz <kortizl88@gmail.com>
2020-12-05 19:58:12 -0600

    Se crea el componente de la fase 5

--- app-prueba/src/App.js
+++ app-prueba/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Botones } from './Botones';
 import { Cambios } from './Cambios';
+import { Fase5 } from './Fase5';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Cambios de la fase 3</h1>
       <h2>estos son los cambios </h2>
       <Cambios />
+      <Fase5 />
       <Botones />
       <h3>Fin</h3>
     </div>
---
+++ app-prueba/src/Fase5.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Fase5 = () => {
+    return (
+        <div>
+            Este es el componente creado en la fase 5
+        </div>
+    )
+}
