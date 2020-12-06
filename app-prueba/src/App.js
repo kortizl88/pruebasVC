5ac6e8e62490e8ecef25f649293e8e72fb4c6714
kristian ortiz <kortizl88@gmail.com>
2020-12-05 20:31:10 -0600

    Cambios de la fase 6 y 7
    Componetente fase 6
    Se agrega el contenedor de imagenes

--- app-prueba/src/App.js
+++ app-prueba/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Botones } from './Botones';
 import { Cambios } from './Cambios';
 import { Fase5 } from './Fase5';
+import { ContenedorImages } from './ContenedorImages';
+import { FaseSeis } from './FaseSeis';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <h2>estos son los cambios </h2>
       <Cambios />
       <Fase5 />
+      <FaseSeis />
+      <ContenedorImages />
       <Botones />
       <h3>Fin</h3>
     </div>
---
+++ app-prueba/src/ContenedorImages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import dbz from './img/dbz.jpeg'
+
+export const ContenedorImages = () => {
+    return (
+        <div>
+         <h1> IMAGENES</h1>
+         <img src={dbz} />
+
+        </div>
+    )
+}
