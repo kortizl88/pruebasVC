ab5e6509945e7156b826423835fb97a19c7b6df1
kristian ortiz <kortizl88@gmail.com>
2020-12-05 18:30:21 -0600

    mis cambios de la fase 2

--- app-prueba/src/App.js
+++ app-prueba/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { Botones } from './Botones';
+import { Cambios } from './Cambios';
 
 function App() {
   return (
     <div className="App">
       <h1>Cambios de la fase 3</h1>
       <h2>estos son los cambios </h2>
+      <Cambios />
       <Botones />
       <h3>Fin</h3>
     </div>
---
+++ app-prueba/src/Cambios.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Cambios = () => {
+    return (
+        <div>
+            Mi cambio consisnte en esta div dentro de un componente
+        </div>
+    )
+}
