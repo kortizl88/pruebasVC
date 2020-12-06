dde89c26264d11354756f36fdf3332ca871a8786
kristian ortiz <kortizl88@gmail.com>
2020-12-05 18:42:26 -0600

    cambios de la fase 3

--- app-prueba/src/App.js
+++ app-prueba/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import { Botones } from './Botones';
 
 function App() {
   return (
     <div className="App">
-      <h1>Cambios de la fase 1</h1>
+      <h1>Cambios de la fase 3</h1>
       <h2>estos son los cambios </h2>
+      <Botones />
       <h3>Fin</h3>
     </div>
   );
